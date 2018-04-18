8875373e00db888e45dde40d786a9884257e55c7
杨玉东 <progyang@gmail.com>
2018-04-18 20:57:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ def strategy(end_date=None):
             return False
         return \
             low_atr.check_low_increase(stock, data)
-            # enter.check_ma(stock, data, end_date=end_date) \
-            # and enter.check_max_price(stock, data, end_date=end_date) \
-            # and enter.check_volume(stock, data, end_date=end_date)
+            enter.check_ma(stock, data, end_date=end_date) \
+            and enter.check_max_price(stock, data, end_date=end_date) \
+            and enter.check_volume(stock, data, end_date=end_date)
     return end_date_filter
 
 

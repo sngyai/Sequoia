c1d47781701470c2c6211bc29ba0461c052f404f
yangyudong <progyang@gmail.com>
2019-03-04 16:42:01 +0800

    消息推送

--- notify.py
+++ notify.py
@@ -4,21 +4,7 @@ import json
 
 
 def notify(msg=None):
-    conn = http.client.HTTPSConnection("sngyai.com:5280")
-
     if msg is None or not msg:
         msg = "今日没有符合条件的股票"
-    payload = json.dumps({"type": "headline",
-                          "from": "admin@sngyai.com",
-                          "to": "sequoia@sngyai.com",
-                          "subject": "investing",
-                          "body": msg})
-
-    headers = {
-        'authorization': "Basic YWRtaW5Ac25neWFpLmNvbTpTTjExMjM1OA==",
-        'cache-control': "no-cache",
-        }
-
-    conn.request("POST", "/api/send_message", payload, headers)
-
-    conn.getresponse()
+    # 可以实现自己的推送逻辑（例如发送到手机上）
+    print(msg)

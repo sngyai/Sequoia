4769f15f4d16a266db9e60039b4890d018205a17
yangyudong <progyang@gmail.com>
2019-03-19 19:12:46 +0800

    添加阶段放量

--- strategy/enter.py
+++ strategy/enter.py
@@ -82,10 +82,45 @@ def check_volume(code_name, data, end_date=None, threshold=60):
 
     mean_vol = total_vol / threshold
     vol_ratio = last_vol / mean_vol
-    if vol_ratio >= 3:
+    if vol_ratio >= 7:
 
         msg = "*{0}({1}) 量比：{2:.2f}\n\t收盘价：{3}\n".format(name, stock, vol_ratio, last_close)
         logging.info(msg)
         return True
     else:
         return False
+
+
+# 量比大于3.0
+def check_continuous_volume(code_name, data, end_date=None, threshold=60, window_size=5):
+    stock = code_name[0]
+    name = code_name[1]
+    total_vol = 0
+    if end_date is not None:
+        mask = (data['date'] <= end_date)
+        data = data.loc[mask]
+    data = data.tail(n=threshold + window_size)
+    if data.size < threshold + window_size:
+        logging.info("{0}:样本小于{1}天...\n".format(stock, threshold+window_size))
+        return False
+
+    # 最后一天收盘价
+    last_close = data.iloc[-1]['close']
+    # 最后一天成交量
+    last_vol = data.iloc[-1]['volume']
+
+    data_front = data.head(n=threshold)
+    data_end = data.tail(n=window_size)
+
+    for index, row in data_front.iterrows():
+        total_vol += float(row['volume'])
+
+    mean_vol = total_vol / threshold
+
+    for index, row in data_end.iterrows():
+        if float(row['volume']) / mean_vol < 3.0:
+            return False
+
+    msg = "*{0}({1}) 量比：{2:.2f}\n\t收盘价：{3}\n".format(name, stock, last_vol/mean_vol, last_close)
+    logging.info(msg)
+    return True

a4b7394628373345e4bcddec02925d1dc2f2e926
yangyudong <progyang@gmail.com>
2019-08-16 11:30:29 +0800

    修改均线多头策略

--- strategy/keep_increasing.py
+++ strategy/keep_increasing.py
@@ -6,12 +6,12 @@ import logging
 from strategy import turtle_trade
 
 
-# 持续上涨（MA60向上）
-def check(code_name, data, end_date=None, threshold=60):
+# 持续上涨（MA30向上）
+def check(code_name, data, end_date=None, threshold=30):
     if len(data) < threshold:
         logging.debug("{0}:样本小于{1}天...\n".format(code_name, threshold))
         return
-    data['ma60'] = pd.Series(tl.MA(data['close'].values, 60), index=data.index.values)
+    data['ma30'] = pd.Series(tl.MA(data['close'].values, 30), index=data.index.values)
 
     begin_date = data.iloc[0].date
     if end_date is not None:
@@ -28,8 +28,8 @@ def check(code_name, data, end_date=None, threshold=60):
     step1 = round(threshold/3)
     step2 = round(threshold*2/3)
 
-    if data.iloc[0]['ma60'] < data.iloc[step1]['ma60'] < \
-        data.iloc[step2]['ma60'] < data.iloc[-1]['ma60']:
+    if data.iloc[0]['ma30'] < data.iloc[step1]['ma30'] < \
+        data.iloc[step2]['ma30'] < data.iloc[-1]['ma30'] and data.iloc[-1]['ma30'] > 1.2*data.iloc[0]['ma30']:
         return True
     else:
         return False

499ef3ab7cf5cc53d4718dace207caff085e99fb
yangyudong <progyang@gmail.com>
2019-08-16 11:40:59 +0800

    未获取到数据时的异常处理

--- work_flow.py
+++ work_flow.py
@@ -26,20 +26,22 @@ def process():
         all_data = ts.get_today_all()
         subset = all_data[['code', 'name', 'nmc']]
         subset.to_csv(settings.STOCKS_FILE, index=None, header=True)
+        stocks = [tuple(x) for x in subset.values]
+        statistics(all_data, stocks)
     except urllib.error.URLError as e:
         subset = pd.read_csv(settings.STOCKS_FILE)
         subset['code'] = subset['code'].astype(str)
+        stocks = [tuple(x) for x in subset.values]
 
-    stocks = [tuple(x) for x in subset.values]
     if utils.need_update_data():
         utils.prepare()
         data_fetcher.run(stocks)
         check_exit()
 
     strategies = {
-        '突破平台': breakthrough_platform.check,
-        # '海龟交易法则': turtle_trade.check_enter,
-        # '放量上涨': enter.check_volume,
+        '海龟交易法则': turtle_trade.check_enter,
+        '放量上涨': enter.check_volume,
+        # '突破平台': breakthrough_platform.check,
         # '均线多头': keep_increasing.check,
         # '停机坪': parking_apron.check,
         # '回踩年线': backtrace_ma250.check,
@@ -52,7 +54,6 @@ def process():
         check(stocks, strategy, strategy_func)
         time.sleep(2)
 
-    statistics(all_data, stocks)
     logging.info("************************ process   end ***************************************")
 
 
